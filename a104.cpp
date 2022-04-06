dbc9475c440b387811c4d11467f53a2fcb2f870a
kuokuo <92105176+huangmayor0905@users.noreply.github.com>
2022-04-06 15:18:14 +0800

    Create a104.cpp

---
+++ a104.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    while (cin >> n) {
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        sort(a, a+n);
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+    }
+}
