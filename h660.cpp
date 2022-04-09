5d261c3823d2649d16723e6b668c2977d302498b
kuokuo <92105176+huangmayor0905@users.noreply.github.com>
2022-04-09 16:38:23 +0800

    Create h660.cpp

---
+++ h660.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, range, v;
+	cin >> x >> range >> v;
+
+	int times;
+	cin >> times;
+
+	for (int i = 0; i < times; i++) {
+		int ball_x, ball_v;
+		cin >> ball_x >> ball_v;
+
+		if ((ball_x >= x - range && ball_x <= x + range) && (ball_v <= v)) {
+			x = ball_x;
+		} else if ((ball_x >= x - range && ball_x <= x + range) && (ball_v > v)) {
+			if (ball_x < x) {
+				x += 15;
+			} else if (ball_x >= x) {
+				x -= 15;
+			}
+		}
+	}
+
+	cout << x << "\n";
+}
